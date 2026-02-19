5a9ac7a5dbec4ee0d83033fdacf8ca65d4c37ab4
manssehtodd <manassehtodd1@gmail.com>
2026-02-19 19:16:04 +0100

    Fix base path configuration in vite.config.js for React plugin

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,8 @@ export default defineConfig({
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
-        base: '/'
       },
+      base: '/',
     }),
     tailwindcss(),
     flowbiteReact()
